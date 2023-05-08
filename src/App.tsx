8ec335d5b59043f460c02538ed0fb03e92fa66a1
Marco Antônio <ma.souza.junior@gmail.com>
2023-05-08 18:47:02 -0300

    fea: run eslint --fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
   return (
     <>
       <div className="flex mx-auto my-12 justify-center gap-4">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a
+          href="https://vitejs.dev"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            alt="Vite logo"
+            className="logo"
+            src={viteLogo}
+          />
         </a>
-        <a href="https://react.dev" target="_blank" className="animate-spin">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          className="animate-spin"
+          href="https://react.dev"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            alt="React logo"
+            className="logo react"
+            src={reactLogo}
+          />
         </a>
       </div>
       <h1 className="text-3xl text-center">Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
